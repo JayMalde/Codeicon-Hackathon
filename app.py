ec3d617c031afd23cfc5ccd669ebad7f89e3b88e
Jay Malde <maldejay77@gmail.com>
2021-11-25 23:19:42 +0530

    Updated Code

--- app.py
+++ app.py
@@ -1,77 +1,112 @@
-from flask import Flask,request,render_template
-import datetime
+import pandas as pd
+from apriori import apriori
+from flask import (Flask, render_template, redirect, request)
 import pymysql
+from csv import writer
+import psycopg2
+dataset = pd.read_csv('data.csv',  header = None )
 
-app = Flask(__name__)
+transactions = []
+list_of_products=[]
+basket=[]
+app=Flask(__name__)
 
-@app.route('/')
-def formLogin():
-    return render_template('login.html')
+totalrows = len(dataset)
+totalcol =int( dataset.size /len(dataset) )
 
-@app.route('/login',methods=['POST'])
-def login():
-    login_id = request.form['login_id']
-    password = request.form['password']
-    type = request.form['type']
-    
-    con = pymysql.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="canteen",
-        port=3306
+for i in range(0, len(dataset)):
+    cart = []
+    for j in range(0,totalcol):
+        if str( dataset.values[i,j] ) != "nan":
+            cart.append( str( dataset.values[i,j]  ))            
+        if str(dataset.values[i,j]) not in list_of_products:
+            list_of_products.append(str(dataset.values[i,j]))  
+    transactions.append(cart)
+
+
+rules = apriori( transactions, min_support = 0.003, min_confidence = 0.04, min_lift = 3)
+results = list(rules)
+
+def recommendation(basket):    
+    recommendations=[]
+    for item in results:
+        pair = item[0] 
+        items = [x for x in pair]
+        for product in basket:
+            if items[0]==product:
+                # print("Rule: " + items[0] + " -> " + items[1])       
+                # print("Support: " + str(item[1]))
+                # print("Confidence: " + str(item[2][0][2]))
+                # print("Lift: " + str(item[2][0][3]))
+                if items[1] not in recommendations:
+                    recommendations.append(items[1])
+    return recommendations
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+    if request.method == 'POST':
+        form_items = request.form.getlist('items')
+        for item in form_items:
+            basket.append(item)
+    context = {
+        'itemset_count': len(list_of_products),
+        'rules_count': len(results),
+        'items': list_of_products,
+        'basket':basket,
+        'recommendations': recommendation(basket),
+    }
+    return render_template('main.html', **context)
+
+@app.route('/reset-basket/', methods=['POST'])
+def reset_basket():
+    global basket
+    basket = []
+    return redirect('/')
+
+@app.route('/menu')
+def menu():
+    con = psycopg2.connect(
+        host="ec2-18-214-243-100.compute-1.amazonaws.com",
+        database="dfi4ih5icfnkkl",
+        user="rmzmvojdioauqd",
+        password="b763ae371dc69381b1f96438a4a4324c7a16f4a1d93808a27e8386c4a6380e45",
+        port=5432    
     )
-    print("connected")
     cur = con.cursor()
-    print("login_id "+login_id+" and password = "+password+" and type "+type)
-    sql = "SELECT COUNT(*) FROM login_data WHERE login_id = %s and password = %s and type = %s"
-    val = (login_id,password,type)
-    cur.execute(sql,val)
+    sql = "select product_name,price,time_required,category from products order by category"
+    cur.execute(sql)
     data = cur.fetchall()
     cur.close()
     con.close()
-    for item in data:
-        print(item[0])
-        if(item[0] == 1):
-            if(type == 'admin'):
-                return render_template('admin.html')
-            else:
-                return render_template('user.html')
-        else:
-            print(type)
-            print(login_id)
-            print(password)
-            print(item[0])
-            return render_template('error.html')
+    return render_template('menu.html',data = data,categoryPrinted = '')
 
-@app.route('/add-products',methods = ['POST'])
-def addProducts():
+@app.route('/add')
+def add():
+    return render_template('add_product.html')
+
+@app.route('/add_product',methods = ['POST'])
+def addProduct():
     timeReq = float(request.form['timeReq'])
     price = float(request.form['price'])
     productName = request.form['productName']
+    productId = request.form['productId']
     category = request.form['category']
-    status = 'Available'
-    productId = int(request.form['productIdActual'])
-    login_id = 1
-    date_of_record = datetime.datetime.now()
-    con = pymysql.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="canteen",
-        port=3306
+    con = psycopg2.connect(
+        host="ec2-18-214-243-100.compute-1.amazonaws.com",
+        database="dfi4ih5icfnkkl",
+        user="rmzmvojdioauqd",
+        password="b763ae371dc69381b1f96438a4a4324c7a16f4a1d93808a27e8386c4a6380e45",
+        port=5432    
     )
     cur = con.cursor()
     try:
         print('Executing SQL')
-        sql = "INSERT INTO products VALUES (%s, %s,%s,%s)"
-        val = (productId,productName,category,status)
-        cur.execute(sql, val)
-        sql = "INSERT INTO price(product_id,price, date_of_record, login_id) VALUES (%s, %s,%s,%s)"
-        val = (productId, price, date_of_record, login_id)
-        cur.execute(sql, val)
-        sql = "INSERT INTO time_required(product_id,time_required, date_of_record, login_id) VALUES (%s, %s,%s,%s)"
-        val = (productId, timeReq, date_of_record, login_id)
+        sql = "INSERT INTO products VALUES (%s,%s,%s,%s,%s)"
+        val = (productId,productName,category,price,timeReq)
         cur.execute(sql, val)
     except pymysql.InternalError as error:
         code, message = error.args
@@ -84,37 +119,33 @@ def addProducts():
     con.close()
     return render_template('success.html')
 
-@app.route('/form-add-products')
-def formAddProducts():
-    con = pymysql.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="canteen",
-        port=3306
+@app.route('/order')
+def order():
+    con = psycopg2.connect(
+        host="ec2-18-214-243-100.compute-1.amazonaws.com",
+        database="dfi4ih5icfnkkl",
+        user="rmzmvojdioauqd",
+        password="b763ae371dc69381b1f96438a4a4324c7a16f4a1d93808a27e8386c4a6380e45",
+        port=5432    
     )
     cur = con.cursor()
-    cur.execute("SELECT COUNT(productId) FROM products")
-    data = cur.fetchone()
-    productId = data[0]+1
-    return render_template('add_products.html',productId = productId)
-
-@app.route('/menu')
-def menu():
-    con = pymysql.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="canteen",
-        port=3306
-    )
-    cur = con.cursor()
-    sql = "select productName,price,time_required,category from products inner join price on products.productId = price.product_id inner join time_required on products.productId = time_required.product_id where status = 'available' order by category"
+    sql = "select product_name,price,time_required,category from products order by category"
     cur.execute(sql)
     data = cur.fetchall()
     cur.close()
     con.close()
-    return render_template('menu.html',data = data,categoryPrinted = '')
+    return render_template('product_list.html',data = data)
+
+@app.route('/add_order_to_list', methods=['GET', 'POST'])
+def add_order():
+    if request.method == 'POST':
+        form_items = request.form.getlist('items')
+        with open('data.csv', 'a',newline='\n') as f_object:
+            writer_object = writer(f_object)
+            writer_object.writerow(form_items)
+            f_object.close()
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+    debug=True
---
+++ postgres.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="ec2-18-214-243-100.compute-1.amazonaws.com",
+    database="dfi4ih5icfnkkl",
+    user="rmzmvojdioauqd",
+    password="b763ae371dc69381b1f96438a4a4324c7a16f4a1d93808a27e8386c4a6380e45",
+    port=5432    
+)
+print("Opened database successfully")
+
+cur = conn.cursor()
+# cur.execute("create table products(product_id int primary key,product_name varchar(100),category varchar(50),price int,time_required int)")
+# print("Table created successfully")
+# conn.commit()
+cur.execute("INSERT INTO products VALUES (1,'dosa','breakfast',80,10)")
+print("Records created successfully")
+conn.commit()
+conn.close()
